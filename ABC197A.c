bcf6fffd1b84e0f50dba8bef630f9fd849b083eb
Andrea <oquendo.3455@gmail.com>
2021-04-21 10:59:11 -0300

    AtCoder Contest

---
+++ ABC197A.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+	char v[3];
+	scanf("%s", v);
+	printf("%c%c%c\n",v[1],v[2],v[0]);
+
+
+}
---
+++ ABC197A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+	string v;
+	cin >> v;
+	printf("%d", v.size());
+
+}
